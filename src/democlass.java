dc1a5e242e40cc129495664a932cb8dcf2ecc175
UPCSM SKILL <walasabkuch@gmail.com>
2021-07-05 11:46:41 +0530

    added print git

--- src/democlass.java
+++ src/democlass.java
@@ -4,6 +4,9 @@ public class democlass {
 
     public static void main(String[] args) {
      System.out.println("hello java ");
+        System.out.println("hello this is my first java class ... ");
+        System.out.println("hello git ");
+
 
     }
 
